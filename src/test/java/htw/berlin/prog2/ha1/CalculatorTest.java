a434bcafe2f55fe2651c2dbf1a7595275b6d9f2c
Hoang <huyhoangdang79@gmail.com>
2026-04-26 16:13:38 +0200

    NEU Teilaufgabe 2 roter Test (2)

--- src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -143,5 +143,20 @@ class CalculatorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    @DisplayName("should display Error when dividing by zero with zero")
+    void testDivisionByZeroAndZero() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(0);
+        calc.pressBinaryOperationKey("/");
+        calc.pressDigitKey(0);
+        calc.pressEqualsKey();
+
+        String expected = "Error";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
 }
 
